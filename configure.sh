196739c41b0963dcc338a2e932e01220d5b46c44
WafiWadud <wafinashwan@gmail.com>
2026-01-03 19:13:51 +0600

    Add debug build.

--- configure.sh
+++ configure.sh
@@ -1,8 +1,37 @@
 #!/bin/sh
-set -eux
 
+set -eu
+
+echo "Select build type:"
+echo "  1) release"
+echo "  2) debug"
+echo "  3) debugoptimized"
+printf "Enter choice [1-3] (default: 1): "
+
+read -r choice || choice=""
+
+set -eux
 BUILD_DIR=build
+
+case "$choice" in
+2)
+	BUILD_TYPE=debug
+	;;
+3)
+	BUILD_TYPE=debugoptimized
+	;;
+"" | 1)
+	BUILD_TYPE=release
+	;;
+*)
+	echo "Invalid choice: $choice"
+	exit 1
+	;;
+esac
+
+echo "Configuring build type: $BUILD_TYPE"
 
 meson setup "$BUILD_DIR" \
 	--native-file clang.ini \
+	--buildtype="$BUILD_TYPE" \
 	--wipe
